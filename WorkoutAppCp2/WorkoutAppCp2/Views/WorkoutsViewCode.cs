955fb6801e51a2c255d24e441e8f5582986eba54
Sai <nagasaimarpaka@gmail.com>
2018-09-19 14:17:46 -0400

    Added Codebehind based ui. Added Grid array, and Button array to keep track of the dynamically generated grids and buttons. Added Grids to dynamically generated "Week" Cards. Added Default Row to the grids added to the Week cards. Added a button as a child to the stack layout of the week card. Attached event handler to the dynamically generated week card button. Added a dictionary to keep track of the week cards grids row counters, to add additional exercises.

--- WorkoutAppCp2/WorkoutAppCp2/Views/WorkoutsViewCode.cs
+++ WorkoutAppCp2/WorkoutAppCp2/Views/WorkoutsViewCode.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.Generic;
+using System.Windows.Input;
 using WorkoutAppCp2.Controls;
 using Xamarin.Forms;
 
@@ -14,9 +15,14 @@ namespace WorkoutAppCp2.Views
         public Button btnAddWeeks;
         public StackLayout sl;
         public ScrollView sv;
+        public int WeekCounter { get; set; }
+        public List<Grid> cardGridList = new List<Grid>();
+        public List<Button> cardGridButtonsList = new List<Button>();
+        public Dictionary<int, int> cardGridCounters = new Dictionary<int, int>();
 
         public WorkoutsViewCode()
         {
+            WeekCounter = 1;
             grid = new Grid
             {
                 VerticalOptions = LayoutOptions.FillAndExpand,
@@ -51,6 +57,7 @@ namespace WorkoutAppCp2.Views
                 VerticalOptions = LayoutOptions.Start,
                 HasShadow = true
             };
+
             btnAddWeeks = new Button
             {
                 Text = "Add Week",
@@ -58,7 +65,7 @@ namespace WorkoutAppCp2.Views
                 VerticalOptions = LayoutOptions.Center,
                 FontAttributes = FontAttributes.Bold,
                 TextColor = Color.White,
-                BackgroundColor = Color.FromHex("#5989B5"),
+                BackgroundColor = Color.FromHex("#5989B5")
             };
             btnAddWeeks.Clicked += BtnAddWeeks_Clicked;
             btnAddWeeks.SetBinding(Button.CommandProperty, "AddWeekCommand");
@@ -68,21 +75,76 @@ namespace WorkoutAppCp2.Views
 
             sl = new StackLayout();
             sl.Spacing = 10;
-            sl.Padding = 10;
+            sl.Padding = 8;
             sl.Children.Add(card);
             sl.Children.Add(btnAddWeeks);
             sv = new ScrollView
             {
                 Content = sl
             };
-
+            sv.VerticalScrollBarVisibility = ScrollBarVisibility.Never;
             Content = sv;
         }
 
         private void BtnAddWeeks_Clicked(object sender, System.EventArgs e)
         {
-            sl.Children.Insert(sl.Children.IndexOf(btnAddWeeks), new CardView { Content = new Label { Text = "Week1" } });
+            Grid cardGrid = new Grid
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = GridLength.Auto }
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = GridLength.Star },
+                    new ColumnDefinition { Width = GridLength.Star }
+                }
+            };
+
+            cardGrid.Children.Add(new Label { Text = "Exercise 1", FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)) }, 0, 0);
+            Button gridButton = new Button
+            {
+                Text = "Add",
+                FontSize = Device.GetNamedSize(NamedSize.Micro, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Center,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.White,
+                BackgroundColor = Color.FromHex("#5989B5"),
+                WidthRequest = 50,
+                HeightRequest = 30
+            };
+            gridButton.Clicked += GridButton_Clicked;
+            cardGridButtonsList.Add(gridButton);
+            cardGridList.Add(cardGrid);
+            StackLayout cardStackLayout = new StackLayout();
+            cardStackLayout.Children.Add(new Label { Text = "Week " + WeekCounter, FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) });
+            cardStackLayout.Children.Add(cardGrid);
+            cardStackLayout.Children.Add(gridButton);
+            sl.Children.Insert(sl.Children.IndexOf(btnAddWeeks), new CardView { Content = cardStackLayout });
             sv.ScrollToAsync(btnAddWeeks, ScrollToPosition.End, false);
+            WeekCounter++;
+        }
+
+        private void GridButton_Clicked(object sender, System.EventArgs e)
+        {
+            int buttonIndex = cardGridButtonsList.IndexOf((Button)sender);
+
+            int currentRowCount;
+            if (cardGridCounters.ContainsKey(buttonIndex))
+            {
+                currentRowCount = cardGridCounters[buttonIndex] + 1;
+                cardGridCounters[buttonIndex] = currentRowCount;
+            }
+            else
+            {
+                currentRowCount = 1;
+                cardGridCounters.Add(buttonIndex, currentRowCount);
+            }
+
+            cardGridList[buttonIndex].Children.Add(new Label { Text = "Exercise "+(currentRowCount+1) }, 0, currentRowCount);
         }
     }
 }
